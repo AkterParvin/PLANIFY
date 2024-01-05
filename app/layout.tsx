2bd2bbf89b25972c67dc72da2a32fa6bb2d7bb4c
connect-Parvin <parvin.akter0190@gmail.com>
2024-01-05 21:14:39 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ["400", "500", "600", "700"],
+  variable:"--font-poppins",
+})
+
+export const metadata: Metadata = {
+  title: 'PLANIFY',
+  description: 'PLANIFY is a platform for event management.',
+  icons: {
+    icon:[""],
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+  )
+}
---
+++ components/shared/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+      <header className="w-full border-b">
+          <div className="wrapper flex items-center justify-between"></div>
+    </header>
+  )
+}
+
+export default Header
